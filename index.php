f9424a1c37bc321f5839443d9880f2ca0b50da06
Maxime <maxim.forriere@gmail.com>
2017-10-21 12:21:58 +0200

    connexion bdd

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>
+<?php
+$servername = "localhost";
+$username = "****";
+$password = "******";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
