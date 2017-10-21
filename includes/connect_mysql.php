afbc74c9885786e997f99b6f5786d16e633d96af
Maxime <maxim.forriere@gmail.com>
2017-10-21 13:44:05 +0200

    connexion error

--- includes/connect_mysql.php
+++ includes/connect_mysql.php
@@ -1,19 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "****";
-$password = "******";
-
-
-function connect()
-{
+function connect(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "Jeankev1337?";
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli("$servername", "$username", "$password");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
     // Check connection
     echo "Connected successfully";
 }
-
 ?>
